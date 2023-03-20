fd3f7e85c613311d68b0dc25fb95bf9cdafdf62a
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-03-20 20:54:16 +0300

    add additional task 2

--- Lesson1/additional_task_2.py
+++ Lesson1/additional_task_2.py
@@ -5,6 +5,6 @@ sum_numbers = 0
 
 while number > 0:
     sum_numbers += number % 10
-    number = number // 10
+    number = number //10
 
 print(f"Сумма чисел трехзначного числа равна {sum_numbers}")
