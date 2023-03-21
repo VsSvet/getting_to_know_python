0160c31877c990e870e4f455ac4697260e464431
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-03-21 16:14:16 +0300

    add task14

--- Lesson2/Task14.py
+++ Lesson2/Task14.py
@@ -1,1 +1,6 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+try:
+    n = int(input("Введите число: "))
+    print(*[2 ** item for item in range(n) if 2 ** item < n])
+except ValueError:
+    print("Вы ввели строку!")
