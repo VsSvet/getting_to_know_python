0854ce04e81834b4e1dec340abd871ffbba16ccb
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-03-23 16:09:23 +0300

    add task1

--- Lesson3/Task1.py
+++ Lesson3/Task1.py
@@ -17,13 +17,10 @@ def convert_input(arg1: str):
 # if __name__ == "__main__":
 #     user_input = input("Введите месяц: ")
 #     if check_input(user_input):
-#         if check_compliance_with_month(user_input):
-#             month = convert_input(user_input)
-#             seasons = ['Зима', 'Зима', 'Весна', 'Весна', 'Весна', 'Лето', 'Лето', 'Лето', 'Осень',
-#                        'Осень', 'Осень', 'Зима']
-#             print(f"Время года: {seasons[month-1]}")
-#         else:
-#             print("Такого месяца нет.")
+#         month = convert_input(user_input) if check_compliance_with_month(user_input) else print("Такого месяца нет.")
+#         seasons = ['Зима', 'Зима', 'Весна', 'Весна', 'Весна', 'Лето', 'Лето', 'Лето',
+#         'Осень','Осень', 'Осень', 'Зима']
+#         print(f"Время года: {seasons[month-1]}")
 #     else:
 #         print("Нужно ввести число.")
 
@@ -32,11 +29,8 @@ def convert_input(arg1: str):
 if __name__ == "__main__":
     user_input = input("Введите месяц: ")
     if check_input(user_input):
-        if check_compliance_with_month(user_input):
-            month = convert_input(user_input)
-            seasons = {'Зима': (12, 1, 2), 'Весна': (3, 4, 5), 'Лето': (6, 7, 8), 'Осень': (9, 10, 11)}
-            [print(f"Время года: {k}") for k, v in seasons.items() if month in v]
-        else:
-            print("Такого месяца нет.")
+        month = convert_input(user_input) if check_compliance_with_month(user_input) else print("Такого месяца нет.")
+        seasons = {'Зима': (12, 1, 2), 'Весна': (3, 4, 5), 'Лето': (6, 7, 8), 'Осень': (9, 10, 11)}
+        [print(f"Время года: {k}") for k, v in seasons.items() if month in v]
     else:
         print("Нужно ввести число.")
